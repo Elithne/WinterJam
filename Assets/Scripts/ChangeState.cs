3597d561668904cb65e1a5b944967b0367d6d66c
Giannina Tartaglia <49578228+Elithne@users.noreply.github.com>
2023-01-16 16:01:00 -0300

    edited compiled error

--- Assets/Scripts/ChangeState.cs
+++ Assets/Scripts/ChangeState.cs
@@ -17,10 +17,7 @@ public class ChangeState : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collider){
         if(collider.CompareTag("Player") && Input.GetKeyDown(KeyCode.Return)){
-<<<<<<< Updated upstream
-=======
             GetComponent<AudioSource>().Play();
->>>>>>> Stashed changes
             this.gameObject.GetComponent<SpriteRenderer>().sprite = spriteChange;
             door.OpenDoor();
             GetComponent<AudioSource>().Play();
